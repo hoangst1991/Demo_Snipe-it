c74f0cc07934fc7cb9856a67cd0373ca365aec54
hoangst1991 <129016314+hoangst1991@users.noreply.github.com>
2025-11-06 21:19:17 +0700

    Delete storage directory

--- storage/framework/views/e3e120318b5e69cfb74b19e9d7d5e2ac.php
+++
@@ -1,9 +0,0 @@
-<tr>
-<td class="header">
-<a href="<?php echo new \Illuminate\Support\EncodedHtmlString($url); ?>">
-<?php echo new \Illuminate\Support\EncodedHtmlString($slot); ?>
-
-</a>
-</td>
-</tr>
-<?php /**PATH C:\xampp\htdocs\snipe-it\resources\views/vendor/mail/html/header.blade.php ENDPATH**/ ?>
